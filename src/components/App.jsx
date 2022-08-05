d0732d6835ceb3e030dec4f4654dd5933fcc9e25
YSquid <ahk648@gmail.com>
2022-08-05 15:14:34 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './app.css'
+
+const App = () => {
+  return (
+    <div className="grid__container">  
+        <div className="grid__item" id="item1">one</div>
+        <div className="grid__item" id="item2">two</div>
+        <div className="grid__item" id="item3">three</div>
+    </div>
+  )
+}
+
+export default App
